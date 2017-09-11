d8316d69b257c73c93440c9bb72fbca5f229e318
sun <zjsunzone@gmail.com>
2017-09-11 16:43:00 +0800

    删除LibNIZK多余代码

--- contracts/nizk/LibNIZK.sol
+++ contracts/nizk/LibNIZK.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.4.2;
 
-import "../utillib/LibString.sol";
-import "../utillib/LibInt.sol";
-import "./LibNizkParam.sol";
+import "LibString.sol";
+import "LibInt.sol";
+import "LibNizkParam.sol";
 library LibNIZK {
     using LibString for *;
     using LibInt for *;
@@ -12,7 +12,7 @@ library LibNIZK {
     function nizk_setup() internal constant returns (string) {
         string memory cmd = "[69d98d6a04c41b4605aacb7bd2f74bee][10nizk_setup]";
 
-        uint rLen = 458880*2; //长度根据库变化
+        uint rLen = 458880*2;
         string memory result = new string(rLen);
 
         uint strptr;
@@ -37,76 +37,6 @@ library LibNIZK {
         return result;
     }
 
-    function nizk_apubcipheradd2(LibNizkParam.NizkParam param) internal returns (string){
-
-        uint ret = nizk_verifyproof(param.pais, param.balapubcipher, param.traapubcipher, param.trabpubcipher, param.apukkey, param.bpukkey, param.nizkpp);
-        if (ret != 0)
-        {
-            return "";
-        }
-
-        string memory cmd = "[69d98d6a04c41b4605aacb7bd2f74bee][18nizk_apubcipheradd]";
-        cmd = cmd.concat("|", param.cipher1);
-        cmd = cmd.concat("|", param.cipher2);
-
-        string memory result = new string(384);
-
-        uint strptr;
-        assembly {
-            strptr := add(result, 0x20)
-        }
-        cmd = cmd.concat("|", strptr.toString());
-
-        bytes32 hash;
-        uint strlen = bytes(cmd).length;
-        assembly {
-            strptr := add(cmd, 0x20)
-            hash := sha3(strptr, strlen)
-        }
-
-        ret = uint(hash);
-        if (ret != 0) {
-            return "";
-        }
-        
-        return result;
-    }
-
-    function nizk_apubciphersub2(LibNizkParam.NizkParam param) internal returns (string){
-        uint ret = nizk_verifyproof(param.pais, param.balapubcipher, param.traapubcipher, param.trabpubcipher, param.apukkey, param.bpukkey, param.nizkpp);
-        if (ret != 0)
-        {
-            return "";
-        }
-
-        string memory cmd = "[69d98d6a04c41b4605aacb7bd2f74bee][18nizk_apubciphersub]";
-        cmd = cmd.concat("|", param.cipher1);
-        cmd = cmd.concat("|", param.cipher2);
-
-        uint rLen = 384; //长度根据库变化
-        string memory result = new string(rLen);
-
-        uint strptr;
-        assembly {
-            strptr := add(result, 0x20)
-        }
-        cmd = cmd.concat("|", strptr.toString());
-
-        bytes32 hash;
-        uint strlen = bytes(cmd).length;
-        assembly {
-            strptr := add(cmd, 0x20)
-            hash := sha3(strptr, strlen)
-        }
-
-        string memory errRet = "";
-        ret = uint(hash);
-        if (ret != 0) {
-            return errRet;
-        }
-        
-        return result;
-    }
 
     function nizk_verifyproof(string pais, string balapubcipher, string traapubcipher, string trabpubcipher, string apukkey, string bpukkey, string nizkpp) internal returns (uint) {
         string memory cmd = "[69d98d6a04c41b4605aacb7bd2f74bee][16nizk_verifyproof]";
@@ -118,15 +48,8 @@ library LibNIZK {
         cmd = cmd.concat("|", bpukkey);
         cmd = cmd.concat("|", nizkpp);
 
-        /* uint rLen = 458880; //长度根据库变化 */
-        /* string memory result = new string(rLen); */
 
         uint strptr;
-        /* assembly { */
-            /* strptr := add(result, 0x20) */
-        /* } */
-        /* cmd = cmd.concat("|", strptr.toString()); */
-
         bytes32 hash;
         uint strlen = bytes(cmd).length;
         assembly {
@@ -139,12 +62,7 @@ library LibNIZK {
     }
 
     function nizk_apubcipheradd(LibNizkParam.NizkParam param) internal returns (string){
-        uint ret;
-//        uint ret = nizk_verifyproof(param.pais, param.balapubcipher, param.traapubcipher, param.trabpubcipher, param.apukkey, param.bpukkey, param.nizkpp);
-//        if (ret != 0)
-//        {
-//            return "";
-//        }
+
 
         string memory cmd = "[69d98d6a04c41b4605aacb7bd2f74bee][18nizk_apubcipheradd]";
         cmd = cmd.concat("|", param.cipher1);
@@ -174,7 +92,7 @@ library LibNIZK {
             hash := sha3(strptr, strlen)
         }
 
-        ret = uint(hash);
+        uint ret = uint(hash);
         if (ret != 0) {
             return "";
         }
@@ -183,18 +101,12 @@ library LibNIZK {
     }
 
     function nizk_apubciphersub(LibNizkParam.NizkParam param) internal returns (string){
-        uint ret;
-//        uint ret = nizk_verifyproof(param.pais, param.balapubcipher, param.traapubcipher, param.trabpubcipher, param.apukkey, param.bpukkey, param.nizkpp);
-//        if (ret != 0)
-//        {
-//            return "";
-//        }
 
         string memory cmd = "[69d98d6a04c41b4605aacb7bd2f74bee][18nizk_apubciphersub]";
         cmd = cmd.concat("|", param.cipher1);
         cmd = cmd.concat("|", param.cipher2);
 
-        uint rLen = 384; //长度根据库变化
+        uint rLen = 384;
         string memory result = new string(rLen);
 
         uint strptr;
@@ -219,7 +131,7 @@ library LibNIZK {
         }
 
         string memory errRet = "";
-        ret = uint(hash);
+        uint ret = uint(hash);
         if (ret != 0) {
             return errRet;
         }
